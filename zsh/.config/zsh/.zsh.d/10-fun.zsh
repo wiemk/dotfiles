7bf14c5dfaa6534acf4c419cfc561c31bfd9b27d
Thorsten Schubert <tschubert@bafh.org>
2014-08-21 22:20:16 +0200

    some more useful zsh functions

--- zsh/.config/zsh/.zsh.d/10-fun.zsh
+++ zsh/.config/zsh/.zsh.d/10-fun.zsh
@@ -1,15 +1,19 @@
-function fun_addandsign() {
-    sudo -- sh -c 'pacman-key -r $1 && pacman-key --lsign-key $1'
+function modified() {
+    pacman -Qii $1 | \
+        while read -r i
+        do
+            [[ $i =~ '^MODIFIED' ]] && echo $i
+        done
 }
 
-function ukill() { 
-    ps x -o  "%r %c " | grep $1 | awk -F' ' '{print $1}' | xargs -I % /bin/kill -TERM -- -%
-}
-
-function awp() {
-    awk '{print $'$1'}';
-}
-
+function fun_addandsign() { sudo -- sh -c 'pacman-key -r $1 && pacman-key --lsign-key $1' }
+function ukill() { ps x -o  "%r %c " | grep $1 | awk -F' ' '{print $1}' | xargs -I % /bin/kill -TERM -- -% }
+function showallmodified() { modified }
+function stripall() { sed -e 's/^[ \t]*//;s/[ \t]*$//' -e '/^$/d' -e '/^\#/d' -- }
+function stripwhitespace() { sed -e 's/^[ \t]*//;s/[ \t]*$//' -- }
+function stripempty() { sed -e '/^$/d' -- }
+function stripcomment() { sed -e '/^\#/d' -- }
+function awp() { awk '{print $'$1'}'; }
 function +() { sudo "$@" }
 function -() { builtin cd .. }
 function @() { cat "$@" }
