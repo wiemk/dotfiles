af8cb42e00b853bc32ea00d63c0abf9853604be9
Thorsten Schubert <tschubert@bafh.org>
2014-08-23 15:26:10 +0200

    some more zsh functions

--- zsh/.config/zsh/.zsh.d/50-dircolors.zsh
+++ zsh/.config/zsh/.zsh.d/50-dircolors.zsh
@@ -1,2 +1,4 @@
+# .zsh.d/50-dircolors.zsh
 eval $(dircolors "${XDG_CONFIG_HOME:-$HOME/.config}"/dircolors)
-
+# EOF
+# vim :set ts=4 sw=4 sts=4 et :
--- zsh/.config/zsh/.zsh.d/90-pkgglob.zsh
+++ zsh/.config/zsh/.zsh.d/90-pkgglob.zsh
@@ -1,4 +1,7 @@
+# .zsh.d/90-pkgglob.zsh
+# depends on pkgfile
 if [ -e /usr/share/doc/pkgfile/command-not-found.zsh ]; then
     source /usr/share/doc/pkgfile/command-not-found.zsh
 fi
-
+# EOF
+# vim :set ts=4 sw=4 sts=4 et :
