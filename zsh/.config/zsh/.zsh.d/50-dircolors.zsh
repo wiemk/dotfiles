d1f065b49aa8d362a42e2fd7cf0b3ffae57471c8
Thorsten Schubert <tschubert@bafh.org>
2014-08-26 17:56:51 +0200

    minor changes: mrconfig + path env var

--- zsh/.config/zsh/.zsh.d/50-dircolors.zsh
+++ zsh/.config/zsh/.zsh.d/50-dircolors.zsh
@@ -1,4 +1,5 @@
 # .zsh.d/50-dircolors.zsh
 eval $(dircolors "${XDG_CONFIG_HOME:-$HOME/.config}"/dircolors)
+
 # EOF
 # vim :set ts=4 sw=4 sts=4 et :
--- zsh/.config/zsh/.zsh.d/90-btrfs.zsh
+++ zsh/.config/zsh/.zsh.d/90-btrfs.zsh
@@ -1,4 +1,5 @@
 # .zsh.d/90-btrfs.zsh
 alias cp='cp -vi --reflink=auto'
+
 # EOF
 # vim :set ts=4 sw=4 sts=4 et :
