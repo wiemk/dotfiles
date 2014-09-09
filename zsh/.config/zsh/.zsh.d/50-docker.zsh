9f6e61769bf3b3bab051ab9a5a432fa3c8111e85
Thorsten Schubert <tschubert@bafh.org>
2014-09-09 12:44:43 +0200

    zsh: modified docker functions

--- zsh/.config/zsh/.zsh.d/50-docker.zsh
+++ zsh/.config/zsh/.zsh.d/50-docker.zsh
@@ -1,9 +1,14 @@
 # .zsh.d/50-docker.zsh
 
 docker-makepkg() {
-    BUILDDIR=$(mktemp -d --tmpdir=$HOME/tmp build-XXXX)
-    docker run --rm -v $(pwd):/build -v $BUILDDIR:/scratch zeno/arch-pkgbuild
-    /bin/rm -rvI $BUILDDIR
+    [[ ! -e PKGBUILD ]] && return 1
+    local BUILDDIR CACHEDIR
+    SCRATCHDIR=$(mktemp -d --tmpdir=$HOME/tmp build-XXXX)
+    CACHEDIR=$(pwd)/pkg
+    mkdir -p $CACHEDIR
+    docker run --rm -v $(pwd):/build -v $CACHEDIR:/pkgcache -v $SCRATCHDIR:/scratch zeno/arch-pkgbuild
+    echo "removing $SCRATCHDIR"
+    /bin/rm -rvI $SCRATCHDIR
 }
 
 docker-build-arch-devel() {
@@ -15,9 +20,11 @@ docker-build-arch-pkgbuild() {
 }
 
 docker-update-build-chain() {
-    docker rmi -f zeno/arch-devel
-    docker rmi -f zeno/arch-pkgbuild
-    docker-build-arch-devel && docker-build-arch-pkgbuild
+    docker rmi -f zeno/arch-devel &&
+    docker rmi -f zeno/arch-pkgbuild &&
+    docker-build-arch-devel &&
+    docker-build-arch-pkgbuild ||
+    echo "Try docker-rmall first or remove the dependant container manually."
 }
 
 # Delete all containers
