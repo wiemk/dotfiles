02767a977ce4535d5ba5f49a9986564cb32e8a59
Thorsten Schubert <tschubert@bafh.org>
2018-10-03 06:12:04 +0200

    fish: fix typo

--- fish/.config/fish/conf.d/ssh-agent.fish
+++ fish/.config/fish/conf.d/ssh-agent.fish
@@ -1,4 +1,4 @@
 set -l target '/mnt/c/Users/zeno/Tools/weasel-pageant/weasel-pageant'
-if test -d $target
+if test -f $target
 	eval (/mnt/c/Users/zeno/Tools/weasel-pageant/weasel-pageant -q -r -S fish)
 end
