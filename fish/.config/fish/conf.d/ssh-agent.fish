72664dc3a678f40f1ebf7736c8d58e571cb9a2b6
Thorsten Schubert <tschubert@bafh.org>
2018-10-03 06:09:22 +0200

    fish: ssh-agent fix

--- fish/.config/fish/conf.d/ssh-agent.fish
+++ fish/.config/fish/conf.d/ssh-agent.fish
@@ -1,1 +1,4 @@
-eval (/mnt/c/Users/zeno/Tools/weasel-pageant/weasel-pageant -q -r -S fish)
+set -l target '/mnt/c/Users/zeno/Tools/weasel-pageant/weasel-pageant'
+if test -d $target
+	eval (/mnt/c/Users/zeno/Tools/weasel-pageant/weasel-pageant -q -r -S fish)
+end
