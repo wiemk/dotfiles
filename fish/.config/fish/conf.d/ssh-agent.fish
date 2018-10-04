320d8b1dc77397156b19ebdf842c3d1fab0d021f
Thorsten Schubert <tschubert@bafh.org>
2018-10-04 08:59:07 +0200

    fish: relay on WSLENV for testing environment

--- fish/.config/fish/conf.d/ssh-agent.fish
+++ fish/.config/fish/conf.d/ssh-agent.fish
@@ -1,5 +1,6 @@
 set -l target '/mnt/c/Users/zeno/Tools/weasel-pageant/weasel-pageant'
-read -l rel < /proc/sys/kernel/osrelease
-if string match -q -- "*Microsoft" $rel -a test -f $target
+#read -l rel < /proc/sys/kernel/osrelease
+#if string match -q -- "*Microsoft" $rel -a test -f $target
+if set -q WSLENV; and test -f $target
 	eval (/mnt/c/Users/zeno/Tools/weasel-pageant/weasel-pageant -q -r -S fish)
 end
