836db41f7f7fda7bc16cb0e92e2b0cfdde972ea9
Thorsten Schubert <tschubert@bafh.org>
2018-10-07 08:53:52 +0200

    fish: add more weasel-pageant logic

--- fish/.config/fish/conf.d/ssh-agent.fish
+++ fish/.config/fish/conf.d/ssh-agent.fish
@@ -1,6 +1,6 @@
-set -l target '/mnt/c/Users/zeno/Tools/weasel-pageant/weasel-pageant'
+# set path to weasel-pageant folder with set -Ux beforehand
 #read -l rel < /proc/sys/kernel/osrelease
-#if string match -q -- "*Microsoft" $rel -a test -f $target
-if set -q WSLENV; and test -f $target
-	eval (/mnt/c/Users/zeno/Tools/weasel-pageant/weasel-pageant -q -r -S fish)
+set -l target "$WEASEL_PATH/weasel-pageant"
+if set -q WSLENV; and test -f "$target"
+	eval (eval $target -a /tmp/ssh-agent.$USER.sock -q -r -S fish)
 end
