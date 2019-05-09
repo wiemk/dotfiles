33b5ebbe4057e3c3474bf7422ab238f21d4ddf7c
Thorsten Schubert <tschubert@bafh.org>
2019-05-09 15:23:15 +0200

    fish: yellow is not very pretty

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,4 +1,7 @@
 set SPACEFISH_PROMPT_ORDER time user dir host git package docker golang rust pyenv exec_time line_sep jobs exit_code char
+set SPACEFISH_EXEC_TIME_COLOR cyan
+set SPACEFISH_TIME_COLOR cyan
+set SPACEFISH_USER_COLOR cyan
 
 if not functions -q fisher
 	set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
