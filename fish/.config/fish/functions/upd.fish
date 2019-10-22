70023185e66edac44ee71dc2778e2e63ad82ba95
Thorsten Schubert <tschubert@bafh.org>
2019-10-23 01:27:42 +0200

    tmux: bind prefix-k for kill-session

--- fish/.config/fish/functions/upd.fish
+++ fish/.config/fish/functions/upd.fish
@@ -1,4 +1,15 @@
 # Defined in - @ line 1
-function upd --description 'alias upd sudo pacman -Syu --noconfirm'
-	sudo pacman -Syu --noconfirm $argv;
+function upd --description 'shortcut for upgrading the system'
+	set -l id (grep -oP '(?<=^ID=).+' /etc/os-release | tr -d '"')
+	switch "$id"
+	case "arch"
+	    if ! command -sq pacman; return 1; end
+		safe_launch "sudo bash -c 'pacman -Syu --noconfirm $argv && pacman -Sc --noconfirm'"
+	case "fedora"
+		if ! command -sq dnf; return 1; end
+		safe_launch "sudo bash -c 'dnf --assumeyes distro-sync; flatpak update --assumeyes'"
+	case *
+		echo "unknown distribution" >&2
+		return 2
+	end
 end
