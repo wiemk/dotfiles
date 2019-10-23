221ab6ccad7ec9bfb8694ec04ac1e873f739442e
Thorsten Schubert <tschubert@bafh.org>
2019-10-23 05:30:51 +0200

    fish: add debian and derivatives to possible targets for the upd function

--- fish/.config/fish/functions/upd.fish
+++ fish/.config/fish/functions/upd.fish
@@ -1,13 +1,19 @@
-# Defined in - @ line 1
 function upd --description 'shortcut for upgrading the system'
-	set -l id (grep -oP '(?<=^ID=).+' /etc/os-release | tr -d '"')
-	switch "$id"
+	set os (grep -oP '(?<=^ID_LIKE=).+' /etc/os-release | tr -d '"')
+	if [ -z "$os" ]
+		set os (grep -oP '(?<=^ID=).+' /etc/os-release | tr -d '"')
+	end
+	
+	switch "$os"
 	case "arch"
 	    if ! command -sq pacman; return 1; end
 		safe_launch "sudo bash -c 'pacman -Syu --noconfirm $argv && pacman -Sc --noconfirm'"
 	case "fedora"
 		if ! command -sq dnf; return 1; end
 		safe_launch "sudo bash -c 'dnf --assumeyes distro-sync; flatpak update --assumeyes'"
+	case "debian"
+		if ! command -sq apt-get; return 1; end
+		safe_launch "sudo bash -c 'apt-get update && apt-get dist-upgrade --assume-yes'"
 	case *
 		echo "unknown distribution" >&2
 		return 2
