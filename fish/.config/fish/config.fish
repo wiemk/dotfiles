94c761e5fef06f53fba5e90fb11b24ec3e400c72
Thorsten Schubert <tschubert@bafh.org>
2019-10-31 18:46:37 +0100

    fish: skim replaces fzy

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,16 +3,3 @@ if not functions -q fisher
 	curl https://raw.githubusercontent.com/jorgebucaran/fisher/master/fisher.fish --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
 	fish -c fisher
 end
-
-if not type -q fzy
-	echo "Please install fzy (https://github.com/jhawthorn/fzy)."
-end
-
-if command -sq starship
-	eval (command starship init fish)
-else
-	set SPACEFISH_PROMPT_ORDER time user dir host git package docker golang rust pyenv exec_time line_sep jobs exit_code char
-	set SPACEFISH_EXEC_TIME_COLOR cyan
-	set SPACEFISH_TIME_COLOR cyan
-	set SPACEFISH_USER_COLOR cyan
-end
