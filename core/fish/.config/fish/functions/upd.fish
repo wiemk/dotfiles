8320f16e7ac3028c45ceb8c02bc19480a14f8117
Thorsten Schubert <tschubert@bafh.org>
2019-11-14 20:08:49 +0100

    Restructure into seperate categories *core* and *gui* for now.

--- core/fish/.config/fish/functions/upd.fish
+++ core/fish/.config/fish/functions/upd.fish
@@ -7,13 +7,13 @@ function upd --description 'shortcut for upgrading the system'
 	switch "$os"
 	case "arch"
 	    if ! command -sq pacman; return 1; end
-		safe_launch "sudo bash -c 'pacman -Syu --noconfirm $argv && pacman -Sc --noconfirm'"
+		safe_launch "sudo bash -c 'pacman -Syu $argv && pacman -Sc --noconfirm'"
 	case "fedora"
 		if ! command -sq dnf; return 1; end
-		safe_launch "sudo bash -c 'dnf --assumeyes distro-sync --refresh'"
+		safe_launch "sudo bash -c 'dnf distro-sync'"
 	case "debian"
 		if ! command -sq apt-get; return 1; end
-		safe_launch "sudo bash -c 'apt-get update && apt-get dist-upgrade --assume-yes'"
+		safe_launch "sudo bash -c 'apt-get update && apt-get dist-upgrade'"
 	case *
 		echo "unsupported distribution" >&2
 		return 2
