cf67a3af79f7a38d622a38633c13fad4e9bc63b1
Thorsten Schubert <tschubert@bafh.org>
2020-11-01 14:26:04 +0100

    tmux: minor style adjustments

--- core/fish/.config/fish/config.fish
+++ core/fish/.config/fish/config.fish
@@ -1,5 +1,6 @@
 if not functions -q fisher
 	set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
-	curl https://raw.githubusercontent.com/jorgebucaran/fisher/master/fisher.fish --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
-	fish -c fisher
+	curl -sL "https://raw.githubusercontent.com/jorgebucaran/fisher/master/fisher.fish" --create-dirs -o "$XDG_CONFIG_HOME/fish/functions/fisher.fish"
+	source "$XDG_CONFIG_HOME/fish/functions/fisher.fish"
+	fisher update
 end
